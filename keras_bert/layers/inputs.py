0dfc60459c1a46aa63fe5675f2138d61910a5995
Yue Zhang <34243681+SchenbergZY@users.noreply.github.com>
2019-07-19 14:33:55 +0200

    Update inputs.py

--- keras_bert/layers/inputs.py
+++ keras_bert/layers/inputs.py
@@ -9,7 +9,23 @@ def get_inputs(seq_len):
     :param seq_len: Length of the sequence or None.
     """
     names = ['Token', 'Segment', 'Masked']
-    return [keras.layers.Input(
+    #a = [keras.layers.Input(
+    #    shape=(seq_len,),
+    #    name='Input-%s' % name,
+    #) for name in names]
+    #return a
+    aa = keras.layers.Input(
         shape=(seq_len,),
-        name='Input-%s' % name,
-    ) for name in names]
+        name='Input-Token',
+    )
+    bb = keras.layers.Input(
+        shape=(seq_len,),
+        name='Input-Segment',
+    )
+    cc = keras.layers.Input(
+        shape=(seq_len,),
+        name='Input-Masked',
+    )
+    aa_mask = keras.layers.Mask(0,name='Input-Token-mask')(aa)
+    bb_mask = keras.layers.Mask(0,name='Input-Segment-mask')(bb)
+    return [aa_mask,bb_mask,cc]
